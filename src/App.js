6e591baf9e7086e23ff9cc62fab4b3ae1303a015
aviexz <alexandroivaldez@gmail.com>
2020-02-12 13:16:18 -0600

    Installed Radium.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Card from "./Card";
+import Radium from "radium";
 
 class App extends Component {
   state = {
--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Radium from "radium";
 
 const card = props => {
   const style = {
